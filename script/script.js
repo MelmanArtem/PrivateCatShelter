a4645c66425b7e685b4816936c3e876aa3e2fe0d
MelmanArtem <122752598+MelmanArtem@users.noreply.github.com>
2023-06-07 16:39:24 +0300

    add changes

--- script/script.js
+++ script/script.js
@@ -12,6 +12,41 @@ const imageCats = [
     'images/Cats/Cat №4.png'
 ]
 
+const infoCats = [
+    {
+        type: "Гладкошерстный",
+        color: "Белый с рыжими глазами",
+        vaccine: "Вакцинированн",
+        passport: "Есть ветпаспорт",
+        things: "Приучена к когтеточке",
+        tray: "Знает лоточек"
+    },
+    {
+        type: "Гладкошерстный",
+        color: "Трехцветная с зелеными глазами",
+        vaccine: "Вакцинированна",
+        passport: "Есть ветпаспорт",
+        things: "Приучен к когтеточке",
+        tray: "Знает лоточек"
+    },
+    {
+        type: "Гладкошерстный",
+        color: "Черно-белый с зелеными глазами",
+        vaccine: "Вакцинированн",
+        passport: "Есть ветпаспорт",
+        things: "Приучен к когтеточке",
+        tray: "Знает лоточек"
+    },
+    {
+        type: "Пушистая",
+        color: "Трехцветная с желыми глазами",
+        vaccine: "Вакцинированна",
+        passport: "Есть ветпаспорт",
+        things: "Приучена к когтеточке",
+        tray: "Знает лоточек"
+    },
+];
+
 for (let i = 0; i < nameCats.length; i++) {
     const dataNameCats = document.createTextNode(nameCats[i]);
     const textCats = document.getElementById(`name-cat-${i+1}`);
@@ -24,3 +59,20 @@ for (let i = 0; i < imagePaths.length; i++) {
     imagePaths[i].src = imageCats[i];
     imagePaths[i].alt = nameCats[i];
 }
+
+for (let i = 0; i < infoCats.length; i++) {
+    const cat = infoCats[i];
+    const infoList = `
+        <ul>
+            <li>${cat.type}</li>
+            <li>${cat.color}</li>
+            <li>${cat.vaccine}</li>
+            <li>${cat.passport}</li>
+            <li>${cat.things}</li>
+            <li>${cat.tray}</li>
+        </ul>
+    `;
+    const targetElement = document.querySelector (`#name-cat-${i+1}`);
+    targetElement.insertAdjacentHTML('afterend', infoList);
+}
+
